7d067274a87098679602ef6b1fce098c63a73c2e
Hammad Ali <49644704+hammad11ali@users.noreply.github.com>
2024-05-30 17:01:59 +0500

    changed project to latest

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'KainatStudio-Frontend' title`, () => {
+  it(`should have the 'Kainat-Studio' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('KainatStudio-Frontend');
+    expect(app.title).toEqual('Kainat-Studio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, KainatStudio-Frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Kainat-Studio');
   });
 });
